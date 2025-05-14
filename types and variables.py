e9adb86c8780d12ffaae094bac1333b0af69e87f
akill005 <146867665+akill005@users.noreply.github.com>
2025-05-14 11:18:09 +0530

    Add python script in spyder

---
+++ types and variables.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+x=5.8
+y=10
+print(x+y)
+print(type(True))
+print((13-4)/(12*12))
+print(type(50))
+print(type(5.6))
+print(int(8.7))
+print(float(98))
